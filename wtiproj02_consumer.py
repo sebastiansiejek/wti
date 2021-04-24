b3bac4b7e4023f3ee973c822b4194f7a28b1dc03
Sebastian Siejek <s.siejek@empressia.pl>
2021-04-24 13:18:08 +0200

    feat:  add async_list

--- wtiproj02_consumer.py
+++ wtiproj02_consumer.py
@@ -9,3 +9,13 @@ class Consumer:
 
     def list(self):
         self.que.list()
+
+    def async_list(self):
+        timeout = time.time() + 10
+
+        while True:
+            print(datetime.datetime.fromtimestamp(
+                time.time()).strftime('%Y-%m-%d %H:%M:%S'), self.list())
+            time.sleep(0.25)
+            if time.time() > timeout:
+                break
