2c9d2a19ff23c7c2dbdab51ed8f560c04b0bf616
Sebastian Siejek <s.siejek@empressia.pl>
2021-06-10 14:14:24 +0200

    feat: add API_CLIENT

---
+++ wtiproj03_API_CLIENT.py
@@ -0,0 +1,37 @@
+import requests
+from API_SERVICE import API_SERVICE
+
+
+class wtiproj03_API_CLIENT:
+
+    def __init__(self):
+        self.enpoint = "http://127.0.0.1:5000/"
+
+    def get(self, id):
+        r = requests.get(self.enpoint + "rating/" + str(id))
+        print("rating/" + str(id), r.status_code)
+
+    def getAll(self):
+        r = requests.get(self.enpoint + "ratings")
+        print('ratings/', r.status_code)
+
+    def delete(self):
+        r = requests.delete(self.enpoint + "ratings")
+        print('delete ratings/', r.status_code)
+
+    def getAvgRatings(self):
+        r = requests.get(self.enpoint + "avg-genre-ratings/all-users")
+        print('avg-genre-ratings/all-users/', r.status_code)
+
+    def getAvgRating(self, id):
+        r = requests.get(self.enpoint + "avg-genre-ratings/user/" + str(id))
+        print('avg-genre-ratings/user/' + str(id), r.status_code)
+
+
+if __name__ == '__main__':
+    client = wtiproj03_API_CLIENT()
+    client.getAll()
+    client.get(78)
+    client.delete()
+    client.getAvgRatings()
+    client.getAvgRating(78)
